f848402f96b2d412434c97af7241aac4fd8f0c44
hoseacodes <dominique11h@yahoo.com>
2021-01-19 12:31:56 -0600

    Added change function

--- src/App.js
+++ src/App.js
@@ -11,16 +11,47 @@ const Input = styled.input`
 `;
 
 class App extends Component {
-  state = {
-    items: ''
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [],
+      newItem: "",
+      pending: true,
+      id: 0
+
+    }
   }
+
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addItem() {
+    const newItem = { text: this.state.newItem.slice(), id: this.state.id + 1 }
+    const list = [...this.state.list]
+    list.push(newItem);
+    this.setState({ list, newItem: "" })
+  }
+
+
   render() {
     return (
       <main>
-        <Input></Input>
-        <Button>Create Button</Button>
+        <Input
+          type="text"
+          name="items"
+          placeholder="Please enter shopping list item"
+          value={this.state.newItem}
+          onChange={e => this.updateInput("newItem", e.target.value)}
+        ></Input>
+        <Button >Create Button</Button>
         {/* Sort alphabetically */}
-        <section> List of Pending Items</section>
+        <section>
+
+          List of Pending Items
+          </section>
         <section> List of Crossed Off Items</section>
         {/* Sort alphabetically */}
 
