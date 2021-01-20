6d3c6a77e5d9887287de57f4be253b451795ecf7
hoseacodes <dominique11h@yahoo.com>
2021-01-20 14:01:02 -0600

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
-import item from './components/item';
+import Header from './components/header';
+import EditForm from './components/editForm';
+import { Button } from 'react-bootstrap';
 
-const Button = styled.button`
-
+const TitleStyle = styled.h2`
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
 `;
-
+const SubTitleStyle = styled.h2`
+    background: whitesome;
+    color: #333;
+    text-align: center;
+    padding: 10px;
+`;
+const InputGroup = styled.div`
+    display: flex;
+`;
+const ListGroupd = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 const Input = styled.input`
-
+    flex: 10;
+    padding: 5px;
 `;
+
+
 
 class App extends Component {
   constructor(props) {
@@ -25,7 +46,7 @@ class App extends Component {
     }
   }
 
-  updateInput(key, value) {
+  updateInput = (key, value) => {
     this.setState({
       [key]: value,
       filteredSearch: value.substr(0, 20),
@@ -67,12 +88,20 @@ class App extends Component {
       complete
     })
   }
-  addItem() {
+  addItem = () => {
     const idx = this.state.id + 1
-    const newItem = { text: this.state.newItem, id: idx, pending: true }
+    const newItem = { text: this.state.newItem, id: idx, pending: true, category: "", quanity: 0, price: 0 }
     const list = [...this.state.list]
     list.push(newItem);
-    this.setState({ list, newItem: "", id: idx, pending: true })
+    this.setState({ list, newItem: "", id: idx, pending: true, category: "", quanity: 0, price: 0 })
+  }
+
+  updateMetadata = (idx) => {
+    const newItem = { text: this.state.newItem, pending: true, category: "", quanity: 0, price: 0 }
+    const list = [...this.state.list]
+    list.push(newItem);
+    this.setState({ list: this.state.list })
+
   }
 
   onSubmit = (e) => {
@@ -87,45 +116,44 @@ class App extends Component {
   render() {
     const { list, complete } = this.state
     const sortedList = list.sort((a, b) => a.text.localeCompare(b.text))
-      .map((item) => <li
+      .map((item) => <ListGroupd><SubTitleStyle
         style={{ textDecoration: item.pending ? "" : "line-through" }}
         onClick={() => this.pendingCompleted(item.id)}
-        key={item.id} >{item.text}</li>);
-
+        key={item.id}>{item.text} </SubTitleStyle><EditForm item={item} updateMetadata={this.updateMetadata}
+          category={item.category} price={item.price} quanity={item.quanity}
+          userInput={this.updateInput} idx={item.id} /> </ListGroupd>);
     const sortedComplete = complete.sort((a, b) => a.text.localeCompare(b.text))
-      .map((item) => <li
+      .map((item) => <SubTitleStyle
         style={{ textDecoration: item.pending ? "" : "line-through" }}
         onClick={() => this.pendingList(item.id)}
-        key={item.id} >{item.text}</li>);
+        key={item.id} >{item.text}</SubTitleStyle>);
 
 
     // const filteredList = list.filter((item) => {
     //   return item.value.toLowerCase().indexOf(this.state.filteredSearch.toLowerCase()) !== -1
     // })
     return (
-      <main>
-        <Input
-          type="text"
-          name="items"
-          placeholder="Please enter shopping list item"
-          value={this.state.newItem}
-          onChange={e => this.updateInput("newItem", e.target.value)}
-        ></Input>
-        <Button onClick={() => this.addItem()}>Create Button</Button>
-        <section>
+      <>
+        <Header />
+        <InputGroup >
+          <Input
+            type="text"
+            name="items"
+            placeholder="Please enter shopping list item"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          ></Input>
+          <Button variant="outline-secondary" onClick={() => this.addItem()}>Create Button</Button>
+        </InputGroup>
+        <TitleStyle>
           List of Pending Items
-          <ul>
-            {sortedList}
-          </ul>
-        </section>
-        <section>
+        </TitleStyle>
+        {sortedList}
+        <TitleStyle>
           List of Crossed Off Items
-            <ul>
-
-            {sortedComplete}
-          </ul>
-        </section>
-      </main>
+        </TitleStyle>
+        {sortedComplete}
+      </>
     )
   }
 }
